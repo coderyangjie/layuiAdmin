2ccaccf33bb03bb0efb7829507b0f24542a5858f
coderyangjie <327409251@qq.com>
2018-08-06 11:26:14 +0800

    修改js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,8 @@
-﻿//Ajax提交
+/**
+ * Created by yangjie on 2018/8/6.
+ */
+
+//Ajax提交
 function AjaxPost(Url,JsonData,LodingFun,ReturnFun) {
     $.ajax({
         type: "post",
@@ -17,7 +21,7 @@ function AjaxPost(Url,JsonData,LodingFun,ReturnFun) {
 //                     //ajax加载中
 //                },
 //                function (data) {
-//                    //ajax返回 
+//                    //ajax返回
 //                    //AjaxErro(data);
 //                })
 
@@ -27,7 +31,7 @@ function ErroAlert(e) {
     var index = layer.alert(e, { icon: 5, time: 2000, offset: 't', closeBtn: 0, title: '错误信息', btn: [], anim: 2, shade: 0 });
     layer.style(index, {
         color: '#777'
-    }); 
+    });
 }
 
 //Ajax 错误返回处理
@@ -62,17 +66,17 @@ function createCode(e) {
     if (code.length != codeLength) {
         createCode(e);
     }
-	if(canGetCookie == 1){
-    	setCookie(e, code, 60 * 60 * 60, '/');
-	}else{
-		return code;
-	}
+    if(canGetCookie == 1){
+        setCookie(e, code, 60 * 60 * 60, '/');
+    }else{
+        return code;
+    }
 }
 
 
-//hours为空字符串时,cookie的生存期至浏览器会话结束。  
-//hours为数字0时,建立的是一个失效的cookie,  
-//这个cookie会覆盖已经建立过的同名、同path的cookie（如果这个cookie存在）。     
+//hours为空字符串时,cookie的生存期至浏览器会话结束。
+//hours为数字0时,建立的是一个失效的cookie,
+//这个cookie会覆盖已经建立过的同名、同path的cookie（如果这个cookie存在）。
 function setCookie(name, value, hours, path) {
     var name = escape(name);
     var value = escape(value);
@@ -82,21 +86,21 @@ function setCookie(name, value, hours, path) {
     _expires = (typeof hours) == "string" ? "" : ";expires=" + expires.toUTCString();
     document.cookie = name + "=" + value + _expires + path;
 }
-//cookie名获取值  
+//cookie名获取值
 function getCookieValue(name) {
     var name = escape(name);
-    //读cookie属性，这将返回文档的所有cookie     
+    //读cookie属性，这将返回文档的所有cookie
     var allcookies = document.cookie;
-    //查找名为name的cookie的开始位置     
+    //查找名为name的cookie的开始位置
     name += "=";
     var pos = allcookies.indexOf(name);
-    //如果找到了具有该名字的cookie，那么提取并使用它的值     
-    if (pos != -1) {    //如果pos值为-1则说明搜索"version="失败     
-        var start = pos + name.length;   //cookie值开始的位置     
-        var end = allcookies.indexOf(";", start); //从cookie值开始的位置起搜索第一个";"的位置,即cookie值结尾的位置     
-        if (end == -1) end = allcookies.length; //如果end值为-1说明cookie列表里只有一个cookie     
-        var value = allcookies.substring(start, end);  //提取cookie的值     
-        return unescape(value);       //对它解码           
+    //如果找到了具有该名字的cookie，那么提取并使用它的值
+    if (pos != -1) {    //如果pos值为-1则说明搜索"version="失败
+        var start = pos + name.length;   //cookie值开始的位置
+        var end = allcookies.indexOf(";", start); //从cookie值开始的位置起搜索第一个";"的位置,即cookie值结尾的位置
+        if (end == -1) end = allcookies.length; //如果end值为-1说明cookie列表里只有一个cookie
+        var value = allcookies.substring(start, end);  //提取cookie的值
+        return unescape(value);       //对它解码
     }
-    else return "-1";    //搜索失败，返回-1  
-}    
+    else return "-1";    //搜索失败，返回-1
+}
