348e5b2563675fd86c564df9a4b6e8cf9e969d0e
coderyangjie <327409251@qq.com>
2018-08-31 11:24:03 +0800

    修改admin1功能

--- admin1/js/index.js
+++ admin1/js/index.js
@@ -75,7 +75,7 @@ layui.config({
             id: 'LAY_layuipro', //设定一个id，防止重复弹出
             btn: ['火速围观'],
             moveType: 1, //拖拽模式，0或者1
-            content: '<div style="padding:15px 20px; text-align:justify; line-height: 22px; text-indent:2em;border-bottom:1px solid #e2e2e2;"><p>最近偶然发现贤心大神的layui框架，瞬间被他的完美样式所吸引，虽然功能不算强大，但毕竟是一个刚刚出现的框架，后面会慢慢完善的。很早之前就想做一套后台模版，但是感觉bootstrop代码的冗余太大，不是非常喜欢，自己写又太累，所以一直闲置了下来。直到遇到了layui我才又燃起了制作一套后台模版的斗志。由于本人只是纯前端，所以页面只是单纯的实现了效果，没有做服务器端的一些处理，可能后期技术跟上了会更新的，如果有什么问题欢迎大家指导。谢谢大家。</p><p>在此特别感谢Beginner和Paco，他们写的框架给了我很好的启发和借鉴。希望有时间可以多多请教。</p></div>',
+            content: '<div style="padding:15px 20px; text-align:justify; line-height: 22px; text-indent:2em;border-bottom:1px solid #e2e2e2;"><p>最近发现贤心大神的layui框架，瞬间被他的完美样式所吸引，决定做一套基于layui框架的后台管理模板，以便以后自己开发时套用，在网上搜了很多基于layui的模板，有些需要收费，有些开源，但代码结构不是很适合自己，于是自己在别人开源代码的基础上开发了属于自己的后台管理模板。</p><p>在此特别感谢网络上开源自己后台管理模板代码的朋友，我也希望自己的后台模板能让更多的朋友看到，后续也会加入自己开发的一些功能。</p></div>',
             success: function(layero){
                 var btn = layero.find('.layui-layer-btn');
                 btn.css('text-align', 'center');
@@ -145,10 +145,10 @@ function donation(){
         area : ['260px', '367px'],
         tab : [{
             title : "微信",
-            content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='images/wechat.jpg'></div>"
+            content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src=''></div>"
         },{
             title : "支付宝",
-            content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='images/alipay.jpg'></div>"
+            content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src=''></div>"
         }]
     })
 }
